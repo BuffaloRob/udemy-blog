fe030131276519900aec953e0d37c7447e59e2ba
buffalorob <peckham.rob@gmail.com>
2019-01-08 09:57:27 -0500

    UserrHeader: - compDidMount() & use of fetchPosts

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
 
   render() {
     const { user } = this.props;
@@ -23,8 +18,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProp = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 }
 
-export default connect(mapStateToProp, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
